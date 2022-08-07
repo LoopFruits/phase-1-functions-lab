bdae529b729f8500942d9fdc2e1f55970cbb5430
LoopFruits <Kensworldc@gmail.com>
2022-08-07 10:27:20 -0400

    commit tree e73fd75505080b6338cb30756c14f39a3e43a3b1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+const blocks = 42;
+const feet = 264;
+
+function distanceFromHqInBlocks(someValue){
+    // returns the nubmer of blocks given a value 
+    if (someValue > 42){
+        return someValue - 42;
+    } else {
+        return 42 - someValue;
+    }
+
+}
+
+
+function distanceFromHqInFeet(someValue){
+    distanceFromHqInBlocks(someValue);
+}
